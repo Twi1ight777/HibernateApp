e9bad50ece74a86197e98526f1e99d52fb348ac1
Mr. T <vlad.chiglinzev@mail.ru>
2025-03-19 13:33:53 +0700

    Hibernate implementation

--- src/main/java/ru/start/springframework/App.java
+++ src/main/java/ru/start/springframework/App.java
@@ -1,13 +1,33 @@
 package ru.start.springframework;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import ru.start.springframework.model.Person;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+// Создаем конфигурацию с таблицей Person
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        // Создаем ServiceRegistry для настройки Hibernate
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+        SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1);
+            System.out.println("Person loaded: " + person.getName() + " " + person.getAge());
+
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
--- src/main/java/ru/start/springframework/model/Person.java
+++ src/main/java/ru/start/springframework/model/Person.java
@@ -1,4 +1,46 @@
 package ru.start.springframework.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Person") // Название таблицы
 public class Person {
+    @Id
+    @Column(name = "id")
+    private int id; // ID персоны
+    @Column(name = "name") // Название столбца имени
+    private String name;
+    @Column(name = "age") // Название столбца возраста
+    private int age;
+
+    public Person() {
+    }
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
